1769382c09a8b1aec8b85a6f66f519bacd751c56
Spivee <spiveehere@gmail.com>
2016-09-22 22:29:53 +1000

    utilize leaf_sequence.h
    ofc my next change will be to un-template-ize this cuz templates are gunna start working completely differently.

--- include/civil/Properties.h
+++ include/civil/Properties.h
@@ -1,3 +1,4 @@
+#include <spw/leaf_sequence.h>
 
 typedef unsigned char byte;
 
@@ -5,22 +6,16 @@ typedef unsigned char byte;
 // output object data as compact bytes
 // take compact bytes and make object + data
 
-class EntityClass 
-{
-	
-};
-
-typedef byte penum;
-
 class PropertyFormat 
 {
 	typedef std::pair<byte, byte> element;
+	typedef format_type::size_type index;
+	//the rest is required for leaf_sequence
 	typedef std::vector<element> format_type;
 	typedef std::vector<PropertyFormat*> family_type;
-	typedef format_type::size_type index
 	
 	PropertyFormat const &parent;
-	index parent_index;
+	index const parent_index;
 	
 	format_type format;
 	std::map<index, family_type> family_lines;
@@ -32,152 +27,38 @@ class PropertyFormat
 	}
 };
 
-struct Property
+typedef spw::leaf_sequence<byte, PropertyFormat> Property;
+
+
+class PropertyTemplate spw::leaf_sequence<std::pair<PTO, thinggggg>, PropertyFormat>;
+class PropertyTemplate::iterator
 {
-	typedef std::vector<byte> branch_type;
-	typedef PropertyFormat::index index;
-	typedef std::map<PropertyFormat*, branch_type> data_type;
-	data_type data;
-	
-	void erase(PropertyFormat* del)
+	Property::iterator base;
+	PropertyTemplateObject::iterator op;
+	iterator& operator++()
 	{
-		if (!data.erase(del))
-			return;
-		std::vector<data_type::iterator> to_recurse = {del};
-		while (to_recurse) 												// while there is still something on which to recurse
-			for (auto& p: to_recurse.pop_back()->first->family_lines)	// remove one thing to recurse, and, for each possible group of children
-				for (PropertyFormat* next: p->second)					// in fact for each child
-					if (data.erase(next))								// search for and delete the branch associated with that child
-						todel.push_back(next);							// and recurse... unless the child wasn't found anyway
+		switch(op->first)
+		{
+		  case PTO_STATIC:
+			++op;
+			break;
+		  case PTO_STATIC_BRANCH:
+			op.rebranch(op->second);
+			//op = PropertyTemplateObject::iterator(op.tree, op.format->child(op.current, op->second));
+			break;
+		  case PTO_BASE:
+			++op;
+			++base;
+			break;
+		  case PTO_BASE_BRANCH:
+			break;
+		}
+		while(
+		  case PTO_TEMPLATE_BRANCH:
+			op.rebranch(*base);
+			++base;
+			break;
 	}
-	void check_branch(PropertyFormat* check)
-	{
-		if (property->data.count(check))
-			return;
-		for (PropertyFormat* sibling: check->parent->family_lines[check->parent_index])
-			erase_branch(sibling);
-		data[child].resize(child->format.size());
-	}
-	
-	class iterator: public std::iterator<std::bidirectional_iterator, byte>
-	{
-		PropertyFormat *format;
-		Property *property;
-		index current;
-		
-		inline data_type& data () const
-		    {return property->data[format];}
-		bool is_reduced () const
-		    {return current < format->format.size();} // returns false even if irreducible.
-		void reduce ()
-		{
-			while (!is_reduced() && format->parent)
-			{
-				current = format->parent_index + 1;
-				format = format->parent;
-			}
-		}
-		inline std::pair<PropertyFormat*, index> reduced ()
-		{
-			reduce();
-			return std::make_pair(format, current);
-		}
-		inline std::pair<PropertyFormat*, current> reduced () const
-		    {return iterator(*this).reduced();} // what a hack
-		
-		
-		inline byte& operator* ()
-		{
-			reduce();
-			return data()[current];
-		}
-		byte& operator* () const
-		{
-			auto x = reduced();
-			return property->data[x.first][x.second];
-		}
-		
-		
-        iterator& operator++ () // prefix ++
-        {
-			reduce();
-			PropertyFormat* child = format->child(current, **this);
-            if (child) /* currently pointing to substructure id */
-			{
-			    format = child;
-				current = 0;
-				property->check_branch(child);
-			}
-			else
-				current++;
-            return *this;
-        }
-		iterator& operator-- () // prefix --
-		{
-			if (current == 0)
-		    {
-				current = format->parent_index;
-				format = format->parent;
-			}
-			else 
-			{
-				current--;
-				for (PropertyFormat* child = format->child(current, **this); child && child->format.size(); child = format->child(current, **this))
-				{
-					current = child->format.size() - 1;
-					format = child;
-					property->check_branch(child);
-				} // un-reduces iterator 
-			}
-		}
-
-        inline iterator operator++ (int)  // postfix ++
-        {
-           iterator ret(*this);
-           ++(*this);
-           return ret;
-        }
-        inline iterator operator-- (int)  // postfix --
-        {
-           iterator ret(*this);
-           --(*this);
-           return ret;
-        }
-		
-		inline bool operator== (iterator comp) const
-    		{return property == comp.property && reduced() == comp.reduced();}
-		inline bool operator!= (iterator comp) const
-		    {return !operator==(comp);}
-		//idk if these are necessary but they have to be exactly the same as the above which is a pain.
-		inline bool operator== (iterator comp)
-    		{return property == comp.property && reduced() == comp.reduced();}
-		inline bool operator!= (iterator comp)
-		    {return !operator==(comp);}
-		
-		iterator(Property *property_r=nullptr, PropertyFormat *root_format=nullptr, index current_c=0)
-		    property(property_r), format(root_format_r), current(current_c)
-		    {}
-		iterator(iterator const &this_c)
-		    iterator(this_c.property, this_c.format, this_c.current)
-			{}
-	};
-	iterator begin()
-	    {return iterator(this, root, 0);}
-	iterator end()
-	    {return iterator(this, root, root->format.size());} //this will try to reduce itself on comparison if (root->parent)
-    struct branch_struct
-	{
-		Property* property;
-		PropertyFormat* root;
-		iterator begin() const
-	        {return iterator(property, root, 0);}
-	    iterator end() const
-	        {return iterator(property, root, root->format.size());}
-	};
-	branch_struct branch (PropertyFormat* root) // allows you to do things with for (byte& x: prop.branch(component)) {...}
-	    {return branch_struct{this, root};} // please tell me I don't need a ctor
 };
 
-class PropertyTemplate {
-	
-};
+
